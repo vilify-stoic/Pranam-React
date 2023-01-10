ef08c70f816c9416b79689efcc3db2d26374fdd6
shushantkeshari <shushant.keshari@nagarro.com>
2023-01-10 16:31:53 +0530

    changes

--- App.js
+++ App.js
@@ -2,28 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import image from "./logo/image.jpg"
 
-// const Title = () => {
-//     return (
-//         <h1 id="title" key="h2">
-//             Namaste React
-//         </h1>
-//     )
-// }
+const Title = () => {
+    return (
+        <h1 id="title" key="h2">
+            Namaste React
+        </h1>
+    )
+}
 
-// const Header = () => {
-//     return (
-//         <header className='title'>
-//             <div>
-//                 {<Title/>}
-//                 <h1>Heading 1</h1>
-//                 <h1>Heading 2</h1>
-//                 <h1>Heading 3</h1>
-//             </div>
-//         </header>
-//     );
-// };
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<Header/>);
+const Header = () => {
+    return (
+        <header className='title'>
+            <div>
+                {<Title/>}
+                <h1>Heading 1</h1>
+                <h1>Heading 2</h1>
+                <h1>Heading 3</h1>
+            </div>
+        </header>
+    );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Header/>);
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
